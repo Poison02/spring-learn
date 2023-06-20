e0166fb93a968bb5f7b5fd91fc26b4f5da113c87
zchhhhh <2069820192@qq.com>
2023-06-20 19:55:42 +0800

    fix: 降低springboot版本为2.6.13，springcloud版本为2021.0.5

--- springcloud/eureka-01/eureka-client-b/src/main/java/cdu/zch/eurekaclientb/EurekaClientBApplication.java
+++ springcloud/eureka-01/eureka-client-b/src/main/java/cdu/zch/eurekaclientb/EurekaClientBApplication.java
@@ -2,8 +2,10 @@ package cdu.zch.eurekaclientb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EurekaClientBApplication {
 
     public static void main(String[] args) {
--- springcloud/eureka-01/eureka-client/src/main/java/cdu/zch/eurekaclient/EurekaClientApplication.java
+++ springcloud/eureka-01/eureka-client/src/main/java/cdu/zch/eurekaclient/EurekaClientApplication.java
@@ -2,9 +2,10 @@ package cdu.zch.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
